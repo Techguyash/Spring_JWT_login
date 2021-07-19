2f0860f35072ad052379cb4ef670fb16b9d9acd9
techguyash <techguyash@gmail.com>
2021-07-19 16:36:03 +0530

    swagger ui is implemented with changes in security conf. swagger version 3.0.0 is not working. System is stable.

--- src/main/java/com/coma/security/config/Swagger.java
+++ src/main/java/com/coma/security/config/Swagger.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 import org.springframework.context.annotation.Bean;
 
-import springfox.documentation.builders.PathSelectors;
+
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -18,12 +18,8 @@ public class Swagger {
     
     @Bean
     public Docket swaggerConf() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()           
-				.apis(RequestHandlerSelectors.basePackage("com.coma.controller"))
-                .paths(PathSelectors.any())
-                .build();
-
+       return new Docket(DocumentationType.SWAGGER_2).select()
+          .apis(RequestHandlerSelectors.basePackage("com.coma")).build();
     }
   
 }
