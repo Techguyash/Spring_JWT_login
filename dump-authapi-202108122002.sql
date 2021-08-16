c8b9eb1b23b8b03791aa4255bce1de74b58e3a77
techguyash <techguyash@gmail.com>
2021-08-16 12:10:02 +0530

    patch

--- dump-authapi-202108122002.sql
+++ dump-authapi-202108122002.sql
@@ -24,17 +24,14 @@ DROP TABLE IF EXISTS `app_user`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `app_user` (
   `id` bigint NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
   `app_user_role` varchar(255) DEFAULT NULL,
   `dob` date DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `enabled` bit(1) NOT NULL,
-  `first_name` varchar(255) DEFAULT NULL,
   `last_login` datetime DEFAULT NULL,
-  `last_name` varchar(255) DEFAULT NULL,
   `locked` bit(1) NOT NULL,
   `password` varchar(255) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `created_on` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -45,7 +42,7 @@ CREATE TABLE `app_user` (
 
 LOCK TABLES `app_user` WRITE;
 /*!40000 ALTER TABLE `app_user` DISABLE KEYS */;
-INSERT INTO `app_user` VALUES (1,'ROLE_ADMIN',NULL,'test@gmail.com',_binary '','test',NULL,'testoo',_binary '\0','$2a$10$im8nf0vjeYWVVaHJB5Mq0.Gnx/SDoF5Z.6s0rVwN.QHsEcnkHXakm',NULL,NULL),(46,NULL,'2021-07-24','techguyash@gmail.com',_binary '\0',NULL,NULL,NULL,_binary '\0','$2a$10$1Y9HS4f.Ak1kFRpPxaMz3euTE0r.y2hQZaC61dEBvi6Ai0uN19GzS','Admin','2021-07-31 15:07:13');
+INSERT INTO `app_user` VALUES (1,'Admin','ROLE_ADMIN','2021-07-24','admin@mail.com',_binary '',NULL,_binary '\0','$2a$10$SAKSvr6r8zxJz0Hi7PutoOcKSQvDw4LN4LCVl6rttSckBXr/eR4JW');
 /*!40000 ALTER TABLE `app_user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -69,7 +66,6 @@ CREATE TABLE `app_user_role` (
 
 LOCK TABLES `app_user_role` WRITE;
 /*!40000 ALTER TABLE `app_user_role` DISABLE KEYS */;
-INSERT INTO `app_user_role` VALUES (1,'ADMIN'),(2,'BILLER'),(3,'USER');
 /*!40000 ALTER TABLE `app_user_role` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -95,7 +91,7 @@ CREATE TABLE `category` (
 
 LOCK TABLES `category` WRITE;
 /*!40000 ALTER TABLE `category` DISABLE KEYS */;
-INSERT INTO `category` VALUES (6,'plastic item','2021-07-23','plastic items'),(7,'food item','2021-07-23','food items'),(8,'electric item','2021-07-23','electric items'),(9,'toys item','2021-07-23','toys items');
+INSERT INTO `category` VALUES (5,'plastic','2021-08-12','non -eatable'),(6,'food','2021-08-12','non -eatable'),(7,'fruits','2021-08-12','eatable'),(8,'vegetable','2021-08-12','eatable'),(9,'grocessory','2021-08-12','eatable');
 /*!40000 ALTER TABLE `category` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -125,7 +121,7 @@ CREATE TABLE `confirmation_token` (
 
 LOCK TABLES `confirmation_token` WRITE;
 /*!40000 ALTER TABLE `confirmation_token` DISABLE KEYS */;
-INSERT INTO `confirmation_token` VALUES (44,NULL,'2021-07-31 15:07:13','2021-07-31 15:22:13','3085cf22-625d-45af-9c22-612d599f8c02',46);
+INSERT INTO `confirmation_token` VALUES (1,'2021-08-12 19:14:27','2021-08-12 19:14:17','2021-08-12 19:29:17','7588c604-8adc-4ba7-98d2-7cfb8cbf03f6',1);
 /*!40000 ALTER TABLE `confirmation_token` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -153,7 +149,7 @@ CREATE TABLE `customer` (
 
 LOCK TABLES `customer` WRITE;
 /*!40000 ALTER TABLE `customer` DISABLE KEYS */;
-INSERT INTO `customer` VALUES (15,'default','2021-07-20','default@mail.com','1234567790','Retail'),(16,'default','1998-08-20','default@mail.com','1234567791','food supplier');
+INSERT INTO `customer` VALUES (10,'default','1998-08-20','default@mail.com','1234567791','food supplier');
 /*!40000 ALTER TABLE `customer` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -167,9 +163,10 @@ DROP TABLE IF EXISTS `discount`;
 CREATE TABLE `discount` (
   `id` int NOT NULL,
   `created_at` date DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `discount_name` varchar(255) DEFAULT NULL,
-  `percentage_val` float NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `discount_name` varchar(255) NOT NULL,
+  `enabled` bit(1) NOT NULL,
+  `percentage_val` int NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -180,6 +177,7 @@ CREATE TABLE `discount` (
 
 LOCK TABLES `discount` WRITE;
 /*!40000 ALTER TABLE `discount` DISABLE KEYS */;
+INSERT INTO `discount` VALUES (0,'2018-12-07','default','default disount',_binary '',0),(2,'2019-02-15','50% off on invoice','Double DHAMAKA',_binary '',50);
 /*!40000 ALTER TABLE `discount` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -201,7 +199,7 @@ CREATE TABLE `hibernate_sequence` (
 
 LOCK TABLES `hibernate_sequence` WRITE;
 /*!40000 ALTER TABLE `hibernate_sequence` DISABLE KEYS */;
-INSERT INTO `hibernate_sequence` VALUES (33);
+INSERT INTO `hibernate_sequence` VALUES (68);
 /*!40000 ALTER TABLE `hibernate_sequence` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -217,16 +215,20 @@ CREATE TABLE `invoice_t` (
   `amount_paid` float NOT NULL,
   `billed_amount` float NOT NULL,
   `billed_at` datetime DEFAULT NULL,
-  `discount` float NOT NULL,
   `payment_type` varchar(255) DEFAULT NULL,
   `biller_no` bigint DEFAULT NULL,
   `customer_id` bigint DEFAULT NULL,
-  `deduction` float NOT NULL,
+  `discount_id` int DEFAULT NULL,
+  `tax_id` int DEFAULT NULL,
   PRIMARY KEY (`invoice_no`),
   KEY `FK1kbd484o2pkke8c0ge50d05o7` (`biller_no`),
   KEY `FKe3t05bslr3jv7hmmuqvohiml3` (`customer_id`),
+  KEY `FKd1mvdxegwelako09kj98hb8hu` (`discount_id`),
+  KEY `FKrlxh70l95viaykg56hkowjxg2` (`tax_id`),
   CONSTRAINT `FK1kbd484o2pkke8c0ge50d05o7` FOREIGN KEY (`biller_no`) REFERENCES `app_user` (`id`),
-  CONSTRAINT `FKe3t05bslr3jv7hmmuqvohiml3` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`)
+  CONSTRAINT `FKd1mvdxegwelako09kj98hb8hu` FOREIGN KEY (`discount_id`) REFERENCES `discount` (`id`),
+  CONSTRAINT `FKe3t05bslr3jv7hmmuqvohiml3` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`),
+  CONSTRAINT `FKrlxh70l95viaykg56hkowjxg2` FOREIGN KEY (`tax_id`) REFERENCES `tax` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -236,7 +238,7 @@ CREATE TABLE `invoice_t` (
 
 LOCK TABLES `invoice_t` WRITE;
 /*!40000 ALTER TABLE `invoice_t` DISABLE KEYS */;
-INSERT INTO `invoice_t` VALUES (24,40,50,'2021-07-23 10:11:31',10,'card',1,15,0),(26,40,50,'2021-07-23 10:12:41',10,'card',1,16,0),(29,1160,1200,'2021-07-23 10:14:36',40,'cash',1,16,0);
+INSERT INTO `invoice_t` VALUES (16,1200,1200,'2021-08-12 19:27:55','cash',1,10,0,0),(20,1200,1200,'2021-08-12 19:28:33','cash',1,10,2,0),(24,600,1200,'2021-08-12 19:34:15','cash',1,10,2,0),(28,1200,1200,'2021-08-12 19:35:18','cash',1,10,0,0),(32,1200,1200,'2021-08-12 19:37:43','cash',1,10,0,1),(36,1200,1200,'2021-08-12 19:40:06','cash',1,10,0,0),(40,1200,1200,'2021-08-12 19:40:33','cash',1,10,0,0),(44,1200,1200,'2021-08-12 19:40:46','cash',1,10,0,0),(48,1200,1200,'2021-08-12 19:41:42','cash',1,10,0,0),(52,1200,1200,'2021-08-12 19:42:42','cash',1,10,0,1),(56,1320,1200,'2021-08-12 19:43:45','cash',1,10,0,1),(60,720,1200,'2021-08-12 19:45:43','cash',1,10,2,1),(64,660,1200,'2021-08-12 19:49:56','cash',1,10,2,1);
 /*!40000 ALTER TABLE `invoice_t` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -270,7 +272,7 @@ CREATE TABLE `products` (
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES (10,'2021-07-23','laptop iteam',50,'laptop ',30,6,4),(11,'2021-07-23','laptop iteam',50,'car toy ',30,7,3),(12,'2021-07-23','laptop iteam',50,'laptop ',30,8,2),(13,'2021-07-23','laptop iteam',50,'laptop ',30,9,2);
+INSERT INTO `products` VALUES (12,'2021-08-12','toys',50,'boys-car ',30,5,3),(13,'2021-08-12','eatalbe',50,'tomato ',30,8,4),(14,'2021-08-12','eatalbe',50,'potato ',30,8,4),(15,'2021-08-12','eatalbe',50,'soya ',30,8,4);
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -333,7 +335,7 @@ CREATE TABLE `salesorder_t` (
 
 LOCK TABLES `salesorder_t` WRITE;
 /*!40000 ALTER TABLE `salesorder_t` DISABLE KEYS */;
-INSERT INTO `salesorder_t` VALUES (25,1,100,100,24,10),(27,1,100,100,26,10),(28,1,100,100,26,11),(30,1,100,100,29,10),(31,4,200,800,29,11),(32,3,100,300,29,13);
+INSERT INTO `salesorder_t` VALUES (17,1,100,100,16,12),(18,4,200,800,16,13),(19,3,100,300,16,14),(21,1,100,100,20,12),(22,4,200,800,20,13),(23,3,100,300,20,14),(25,1,100,100,24,12),(26,4,200,800,24,13),(27,3,100,300,24,14),(29,1,100,100,28,12),(30,4,200,800,28,13),(31,3,100,300,28,14),(33,1,100,100,32,12),(34,4,200,800,32,13),(35,3,100,300,32,14),(37,1,100,100,36,12),(38,4,200,800,36,13),(39,3,100,300,36,14),(41,1,100,100,40,12),(42,4,200,800,40,13),(43,3,100,300,40,14),(45,1,100,100,44,12),(46,4,200,800,44,13),(47,3,100,300,44,14),(49,1,100,100,48,12),(50,4,200,800,48,13),(51,3,100,300,48,14),(53,1,100,100,52,12),(54,4,200,800,52,13),(55,3,100,300,52,14),(57,1,100,100,56,12),(58,4,200,800,56,13),(59,3,100,300,56,14),(61,1,100,100,60,12),(62,4,200,800,60,13),(63,3,100,300,60,14),(65,1,100,100,64,12),(66,4,200,800,64,13),(67,3,100,300,64,14);
 /*!40000 ALTER TABLE `salesorder_t` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -355,7 +357,7 @@ CREATE TABLE `seq_email_token_id` (
 
 LOCK TABLES `seq_email_token_id` WRITE;
 /*!40000 ALTER TABLE `seq_email_token_id` DISABLE KEYS */;
-INSERT INTO `seq_email_token_id` VALUES (45);
+INSERT INTO `seq_email_token_id` VALUES (2);
 /*!40000 ALTER TABLE `seq_email_token_id` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -377,7 +379,7 @@ CREATE TABLE `seq_user_id` (
 
 LOCK TABLES `seq_user_id` WRITE;
 /*!40000 ALTER TABLE `seq_user_id` DISABLE KEYS */;
-INSERT INTO `seq_user_id` VALUES (47);
+INSERT INTO `seq_user_id` VALUES (2);
 /*!40000 ALTER TABLE `seq_user_id` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -391,9 +393,10 @@ DROP TABLE IF EXISTS `tax`;
 CREATE TABLE `tax` (
   `id` int NOT NULL,
   `created_at` date DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `percentage` float NOT NULL,
-  `tax_name` varchar(255) DEFAULT NULL,
+  `description` varchar(255) NOT NULL,
+  `enabled` bit(1) NOT NULL,
+  `percentage` int NOT NULL,
+  `tax_name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -404,6 +407,7 @@ CREATE TABLE `tax` (
 
 LOCK TABLES `tax` WRITE;
 /*!40000 ALTER TABLE `tax` DISABLE KEYS */;
+INSERT INTO `tax` VALUES (0,'2015-05-05','default',_binary '',0,'default tax'),(1,'2021-05-05','dummy',_binary '',10,'dummy');
 /*!40000 ALTER TABLE `tax` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -429,7 +433,7 @@ CREATE TABLE `units` (
 
 LOCK TABLES `units` WRITE;
 /*!40000 ALTER TABLE `units` DISABLE KEYS */;
-INSERT INTO `units` VALUES (1,'used to measure','ton','tone'),(2,'used to measure','kg','kg'),(3,'used to measure','ml','millie'),(4,'used to measure','ltr','liter'),(5,'used to measure','grm','gram');
+INSERT INTO `units` VALUES (1,'used to measure','grm','gram'),(2,'used to measure','ml','ml'),(3,'used to measure','pc','piece'),(4,'used to measure','kg','kg');
 /*!40000 ALTER TABLE `units` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -458,6 +462,7 @@ CREATE TABLE `vendor` (
 
 LOCK TABLES `vendor` WRITE;
 /*!40000 ALTER TABLE `vendor` DISABLE KEYS */;
+INSERT INTO `vendor` VALUES (11,'164 Erode','2019-10-05','hai@gmail.com','455475','223456890','vendor1');
 /*!40000 ALTER TABLE `vendor` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -474,4 +479,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-07-31 17:57:21
+-- Dump completed on 2021-08-12 20:02:45
